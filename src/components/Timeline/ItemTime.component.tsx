a3032404df3962be30585cb33c414cd670e6124f
Joel Viloria <viloriajoel07@github.com>
2023-04-20 10:46:27 -0500

    corrigiendo titulos del timeline

--- src/components/Timeline/ItemTime.component.tsx
+++ src/components/Timeline/ItemTime.component.tsx
@@ -14,10 +14,8 @@ const ItemTime: FC<itemTime> = ({ data }) => {
   return (
     <li className="mb-10 ml-8">
       <SetIconTime />
-      <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-        {data.title}
-      </h3>
-      <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+      <h3 className="mb-1 text-lg font-semibold text-white">{data.title}</h3>
+      <time className="block mb-2 text-sm font-normal leading-none text-gray-400">
         {data.date}
       </time>
       <p className="text-base font-normal text-gray-300">{data.description}</p>
